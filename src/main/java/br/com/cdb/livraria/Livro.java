7fdeb709d6f3c78eed1c37ee38ac5d28198890e9
Emerson Pessoa da Silva <emersonpessoa05102008@gmail.com>
2025-06-01 12:31:57 -0300

    feat: "Initial project :sparkles:"

---
+++ src/main/java/br/com/cdb/livraria/Livro.java
@@ -0,0 +1,47 @@
+package br.com.cdb.livraria;
+
+public class Livro {
+    private String titulo;
+    private String autor;
+    private String isbn;
+    private double preco;
+
+    
+    public Livro(String titulo, String autor, String isbn, double preco) {
+        this.titulo = titulo;
+        this.autor = autor;
+        this.isbn = isbn;
+        this.preco = preco;
+    }
+    
+    public Livro() {
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+    public String getAutor() {
+        return autor;
+    }
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+    public String getIsbn() {
+        return isbn;
+    }
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+    public double getPreco() {
+        return preco;
+    }
+    public void setPreco(double preco) {
+        this.preco = preco;
+    }
+
+    
+    
+}

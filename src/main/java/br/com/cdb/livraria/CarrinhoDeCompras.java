79ef5a2677b77322f70d2e4def259a46a756875c
emersonpessoa <emersonpessoa05102008@gmail.com>
2025-06-01 19:33:31 -0300

    feat: Added Inclusion Polymorphism :rocket:

--- src/main/java/br/com/cdb/livraria/CarrinhoDeCompras.java
+++ src/main/java/br/com/cdb/livraria/CarrinhoDeCompras.java
@@ -15,7 +15,12 @@ public class CarrinhoDeCompras {
         // Calcula o total do carrinho
         double total = 0;
         for (Livro livro : livros) {
-            total += livro.calcularPrecoTota();
+            //total += livro.calcularPrecoTota();
+            total += livro.getPreco(); // Usando o método getPreco() para obter o preço do livro
+            if (livro instanceof LivroFisico) {
+                total += ((LivroFisico) livro).getFrete(); // Adiciona o frete se for um LivroFisico
+            }
+
         }
 
         return total;
